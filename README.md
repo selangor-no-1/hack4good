7b1009711ae9bdd9c358e2c956ccefd3c76f3248
hewliyang <hewliyang@u.nus.edu>
2024-02-03 16:55:13 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# hack4good
+
+**django project**
+
+```bash
+cd server
+virtualenv .venv
+source .venv/bin/activate
+pip install -r requirements.txt
+```
+
+**sveltekit**
+
+```bash
+pnpm i
+pnpm run dev
+```
---
+++ ui/src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import { Button } from "$lib/components/ui/button";
+</script>
+
+<h1 class="text-3xl">Welcome to SvelteKit</h1>
+<p>
+  Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+</p>
+<Button>Hi</Button>
