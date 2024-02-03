d74652919463c5f54afc32c230c33cf1a8294990
chriswengqi <chris_wengqi@hotmail.com>
2024-02-04 03:15:35 +0800

    simple form interface

---
+++ ui/src/routes/registration/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import type { PageData } from "./$types";
+  import RegistrationForm from "./registration-form.svelte";
+  export let data: PageData;
+</script>
+
+<div class="mx-auto max-w-screen-sm">
+  Registration
+  <RegistrationForm form={data.form} />
+</div>
