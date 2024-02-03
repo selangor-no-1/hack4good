35110da572e1ef3bc8d5b9d81878b7a1644c5b8d
hewliyang <hewliyang@u.nus.edu>
2024-02-03 17:34:41 +0800

    remove /lib from .gitignore

--- server/api/admin.py
+++ server/api/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from api.models import Event, Volunteer
 # Register your models here.
+
+admin.site.register(Event)
+admin.site.register(Volunteer)
--- server/api/models.py
+++ server/api/models.py
@@ -1,3 +1,23 @@
+import uuid
 from django.db import models
 
-# Create your models here.
+
+class Event(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.CharField(max_length=2000)
+    date = models.DateField()
+    volunteers = models.ManyToManyField("Volunteer", related_name="events", blank=True)
+
+    def __str__(self):
+        return f"Event(name={self.name})"
+
+
+class Volunteer(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=200)
+    date_of_birth = models.DateField()
+    email = models.EmailField()
+    contact_number = models.CharField(max_length=8)
+
+    def __str__(self):
+        return f"Volunteer(name={self.name})"
