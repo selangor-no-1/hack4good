1ef200d3d5981a0ec61350132a8a3503ccdf1a5c
Brexton Ho <e0564446@u.nus.edu>
2024-02-04 15:26:20 +0800

    analytics dashboard calendar

--- ui/src/routes/+page.svelte
+++ ui/src/routes/+page.svelte
@@ -1,7 +1,10 @@
 <script lang="ts">
+	import { Activity, CreditCard, Users } from "lucide-svelte";
 	import { fade } from "svelte/transition";
 	import Chart from "chart.js/auto";
 	import { Button } from "$lib/components/ui/button";
+	import * as Card from "$lib/components/ui/card";
+	import DateRangePicker from "$lib/DateRangePicker.svelte";;
 
 	export let data;
 	let ctx: HTMLCanvasElement | undefined;
@@ -54,40 +57,105 @@
 	}
 </script>
 
-<h1 class="text-xl md:text-2xl font-bold mb-4">Dashboard</h1>
-<div class="w-[600px] h-[400px] border rounded-lg p-3">
-	<canvas bind:this={ctx} width="100" height="50" in:fade />
-</div>
-<div>
-	<select bind:value={chartType}>
-		<option value="line">Line</option>
-		<option value="bar">Bar</option>
-	</select>
+
+<div class="flex items-center justify-between space-y-2">
+	<h2 class="text-3xl font-bold tracking-tight">Dashboard</h2>
+	<div class="flex items-center space-x-2">
+		<DateRangePicker />
+
+	</div>
 </div>
 
-<div>
-	<label for="age">
-		<input type="checkbox" bind:checked={showAge} id="age" />
-		<span>Show age</span>
-	</label>
+<div class="grid gap-4 md:grid-cols-2 pt-5 lg:grid-cols-4">
+	<Card.Root>
+		<Card.Header
+			class="flex flex-row items-center justify-between space-y-0 pb-2"
+		>
+			<Card.Title class="text-sm font-medium">Total Manhours</Card.Title>
+			<Users class="h-4 w-4 text-muted-foreground" />
+		</Card.Header>
+		<Card.Content>
+			<div class="text-2xl font-bold">+1,350</div>
+			<p class="text-xs text-muted-foreground">+180.1% from last month</p>
+		</Card.Content>
+	</Card.Root>
+	<Card.Root>
+		<Card.Header
+			class="flex flex-row items-center justify-between space-y-0 pb-2"
+		>
+			<Card.Title class="text-sm font-medium">Total Volunteers</Card.Title>
+			<Users class="h-4 w-4 text-muted-foreground" />
+		</Card.Header>
+		<Card.Content>
+			<div class="text-2xl font-bold">+234</div>
+			<p class="text-xs text-muted-foreground">+19% from last month</p>
+		</Card.Content>
+	</Card.Root>
+	<Card.Root>
+		<Card.Header
+			class="flex flex-row items-center justify-between space-y-0 pb-2"
+		>
+			<Card.Title class="text-sm font-medium">Average Satisfaction</Card.Title>
+			<Activity class="h-4 w-4 text-muted-foreground" />
+		</Card.Header>
+		<Card.Content>
+			<div class="text-2xl font-bold">4.73</div>
+			<p class="text-xs text-muted-foreground">+0.12 since last week</p>
+		</Card.Content>
+	</Card.Root>
 </div>
 
-<div>
-	<label for="height">
-		<input type="checkbox" bind:checked={showHeight} id="height" />
-		<span>Show height</span>
-	</label>
+<div class="grid gap-4 mt-3 md:grid-cols-2 lg:grid-cols-7">
+	<Card.Root class="col-span-5">
+		<Card.Header>
+			<Card.Title>Overview</Card.Title>
+		</Card.Header>
+		<Card.Content>
+			<div class="w-[600px] h-[400px] border rounded-lg mt-5 p-3">
+				<canvas bind:this={ctx} width="100" height="50" in:fade />
+			</div>
+		</Card.Content>
+	</Card.Root>
+	<div class="pt-5 ms-2 col-span-2">
+		<select bind:value={chartType}>
+			<option value="line">Line</option>
+			<option value="bar">Bar</option>
+		</select>
+
+		<div class="pt-5">
+			<div>
+				<label for="age">
+					<input type="checkbox" bind:checked={showAge} id="age" />
+					<span>Show age</span>
+				</label>
+			</div>
+		
+			<div>
+				<label for="height">
+					<input type="checkbox" bind:checked={showHeight} id="height" />
+					<span>Show height</span>
+				</label>
+			</div>
+		
+			<div>
+				<label for="weight">
+					<input type="checkbox" bind:checked={showWeight} id="weight" />
+					<span>Show Weight</span>
+				</label>
+			</div>
+		</div>
+		
+		<div class="pt-5">
+		{#if ctx}
+			<a href={ctx.toDataURL()} target="_blank" download="graph.png">
+				<Button>Download Chart</Button>
+			</a>
+		{/if}
+		</div>
+	</div>
 </div>
 
-<div>
-	<label for="weight">
-		<input type="checkbox" bind:checked={showWeight} id="weight" />
-		<span>Show Weight</span>
-	</label>
-</div>
 
-{#if ctx}
-	<a href={ctx.toDataURL()} target="_blank" download="graph.png">
-		<Button>Download Chart</Button>
-	</a>
-{/if}
+
+
+
