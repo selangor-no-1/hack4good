d7f00ebab47164582e281b2e65da89c715d6a479
chriswengqi <67186918+chriswengqi@users.noreply.github.com>
2024-02-04 20:33:21 +0800

    feat: volunteer page (#7)
    * add volunteer details page
    
    * remove console log
    
    * link names to volunteer profile

---
+++ ui/src/routes/volunteer/[slug]/+page.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import type { Volunteer } from "$lib/types.js";
+
+  export let data;
+
+  const { volunteer }: { volunteer: Volunteer } = data;
+</script>
+
+<main class="flex justify-center items-center h-screen">
+  <div class="flex flex-col items-center">
+    <div class="rounded-full overflow-hidden w-32 h-32 border-4 border-white">
+      <img src="https://avatars.githubusercontent.com/u/88436113?v=4" alt="" />
+    </div>
+    <div class="mt-4 text-center">
+      <h2 class="text-2xl font-semibold text-gray-800">{volunteer.name}</h2>
+      <p class="my-2 text-gray-600">Volunteer</p>
+      <p class="my-2 text-gray-600">Email: {volunteer.email}</p>
+      <p class="my-2 text-gray-600">
+        Contact Number: {volunteer.contact_number}
+      </p>
+      <p class="my-2 text-gray-600">Date of Birth: {volunteer.date_of_birth}</p>
+    </div>
+  </div>
+</main>
