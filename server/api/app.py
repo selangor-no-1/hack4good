0842b17ab470fc39cc4869dacfeecb47c37a3e53
Li Yang <76434265+hewliyang@users.noreply.github.com>
2024-02-04 16:36:44 +0800

    api: volunteer crud & analytics (manhours) (#6)
    * chore: fetch events from server
    
    * feat: volunteer crud & analytics (1/X)
    
    * chore: update sample data

--- server/api/app.py
+++ server/api/app.py
@@ -1,7 +1,15 @@
-from typing import List
+from collections import defaultdict
+import datetime as _dt
+from datetime import datetime
+from typing import List, Optional
 from ninja_extra import NinjaExtraAPI, api_controller, route
 from api.models import Volunteer, Event
-from api.schemas import VolunteerSchema, EventSchema, Error
+from api.schemas import (
+    ManhoursUnit,
+    VolunteerSchema,
+    EventSchemaWithoutVolunteers,
+    Error,
+)
 from django.shortcuts import get_object_or_404
 
 app = NinjaExtraAPI()
@@ -27,25 +35,84 @@ class VolunteerController:
 
 @api_controller("events/", tags=["Events"], permissions=[])
 class EventController:
-    @route.get("/", response=List[EventSchema])
-    def get_events(self):
-        return Event.objects.all()
+    @route.get("/", response=List[EventSchemaWithoutVolunteers])
+    def get_events(
+        self,
+        location: Optional[str] = None,
+        before_date: Optional[_dt.date] = None,
+    ):
+        Q = Event.objects.all()
+        if location:
+            Q = Q.filter(location=location)
+        if before_date:
+            Q = Q.filter(date__lt=before_date)
+        return Q
 
-    @route.get("{slug}", response=EventSchema)
+    @route.get("{slug}", response=EventSchemaWithoutVolunteers)
     def get_event(self, slug: str):
         event = get_object_or_404(Event, slug=slug)
         return event
 
-    @route.post("/", response=EventSchema)
-    def create_event(self, props: EventSchema):
+    @route.get("{slug}/volunteers", response=List[VolunteerSchema])
+    def get_event_volunteers(self, slug: str):
+        event = get_object_or_404(Event, slug=slug)
+        return event.volunteers
+
+    @route.post("/", response=EventSchemaWithoutVolunteers)
+    def create_event(self, props: EventSchemaWithoutVolunteers):
         props = props.model_dump()
         event = Event.objects.create(**props)
         return event
 
+    @route.post("{slug}/volunteers", response=VolunteerSchema)
+    def add_volunteer_to_event(self, slug: str, volunteer_id: str):
+        if not volunteer_id:
+            return 404, "You must specify which Volunteer you want to add!"
+
+        volunteer = get_object_or_404(Volunteer, id=volunteer_id)
+
+        if not volunteer:
+            return (
+                404,
+                "You cannot add a Volunteer that does not exist! Create the Volunteer first before calling this endpoint!",
+            )
+        event = get_object_or_404(Event, slug=slug)
+
+        # update event.volunteers
+        new_volunteers = [*event.volunteers.all(), volunteer]
+        event.volunteers.set(new_volunteers)
+        event.save()
+        return volunteer
+
 
 @api_controller("analytics/", tags=["Analytics"], permissions=[])
 class AnalyticsController:
-    ...
+    @route.get("manhours", response=List[ManhoursUnit])
+    def get_manhours(
+        self,
+        event_slug: Optional[str] = None,
+        location: Optional[str] = None,
+        before_date: Optional[_dt.date] = datetime.now().date(),
+    ):
+        Q = Event.objects.all()
+        if location:
+            Q = Q.filter(location=location)
+        if before_date:
+            Q = Q.filter(date__lt=before_date)
+        if event_slug:
+            Q = Q.filter(slug=event_slug)
+
+        res = defaultdict(lambda: 0)
+        for event in Q.all():
+            event_date = event.date
+            time_diff = event.end_time - event.start_time
+            total_hrs = time_diff.total_seconds() / 3600
+            res[event_date] += total_hrs
+
+        ts = []
+        for date, hours in res.items():
+            ts.append(ManhoursUnit(date=date, hours=hours))
+        return ts
 
 
 app.register_controllers(VolunteerController, EventController, AnalyticsController)
--- server/api/models.py
+++ server/api/models.py
@@ -6,9 +6,12 @@ from django_extensions.db.fields import AutoSlugField
 class Event(models.Model):
     name = models.CharField(max_length=200)
     slug = AutoSlugField(populate_from="name")
-    description = models.CharField(max_length=2000)
+    description = models.CharField(max_length=2000, null=True, blank=True)
+    location = models.CharField(max_length=200, null=True, blank=True)
     image = models.CharField(max_length=200, null=True, blank=True)
-    date = models.DateField()
+    date = models.DateField(null=True, blank=True)
+    start_time = models.DateTimeField(null=True, blank=True)
+    end_time = models.DateTimeField(null=True, blank=True)
     volunteers = models.ManyToManyField("Volunteer", related_name="events", blank=True)
 
     def __str__(self):
@@ -18,9 +21,9 @@ class Event(models.Model):
 class Volunteer(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=200)
-    date_of_birth = models.DateField()
-    email = models.EmailField()
-    contact_number = models.CharField(max_length=8)
+    date_of_birth = models.DateField(null=True, blank=True)
+    email = models.EmailField(null=True, blank=True)
+    contact_number = models.CharField(max_length=8, null=True, blank=True)
 
     def __str__(self):
         return f"Volunteer(name={self.name})"
