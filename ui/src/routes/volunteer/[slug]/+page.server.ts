459a746aa1c77028a63865e3c444540d2aa101d0
chriswengqi <chris_wengqi@hotmail.com>
2024-02-06 01:45:47 +0800

    add events in volunteer details

--- ui/src/routes/volunteer/[slug]/+page.server.ts
+++ ui/src/routes/volunteer/[slug]/+page.server.ts
@@ -1,9 +1,15 @@
 import { API_ENDPOINT } from "$lib/config"; // this should be a secret
 
 export async function load({ params, fetch }) {
-	const res = await fetch(API_ENDPOINT + "volunteers/" + params.slug);
-	const volunteer = await res.json();
+	const volunteer_res = await fetch(API_ENDPOINT + "volunteers/" + params.slug);
+	const events_res = await fetch(API_ENDPOINT + "volunteers/" + params.slug + "/events");
+	const volunteer = await volunteer_res.json();
+	const events = await events_res.json();
+	const event_list = events.events;
+	const hours = events.hours;
 	return {
 		volunteer,
+		event_list,
+		hours
 	};
 }
