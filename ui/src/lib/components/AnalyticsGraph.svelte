f12815326e6dc0f3b7fe86ee6ead762716ca9a12
Li Yang <76434265+hewliyang@users.noreply.github.com>
2024-02-06 18:38:50 +0800

    chore: chart styling (#24)

--- ui/src/lib/components/AnalyticsGraph.svelte
+++ ui/src/lib/components/AnalyticsGraph.svelte
@@ -1,8 +1,14 @@
 <script lang="ts">
 	import { fade } from "svelte/transition";
-	import Chart from "chart.js/auto";
 	import { Button } from "$lib/components/ui/button";
+	import { Checkbox } from "$lib/components/ui/checkbox";
+	import { Label } from "$lib/components/ui/label";
 	import type { User } from "$lib/types";
+	import * as Select from "$lib/components/ui/select";
+	import CrosshairPlugin from "chartjs-plugin-crosshair";
+	import Chart from "chart.js/auto";
+	import * as Card from "$lib/components/ui/card";
+	import { Download } from "lucide-svelte";
 
 	export let userData: User[];
 	let ctx: HTMLCanvasElement | undefined;
@@ -11,6 +17,9 @@
 	let showWeight = true;
 	let showHeight = true;
 	let chartType: "line" | "bar" = "line";
+
+	// register plugins
+	Chart.register(CrosshairPlugin);
 
 	$: if (ctx) {
 		if (chart) {
@@ -43,56 +52,150 @@
 				],
 			},
 			options: {
-				animation: false,
+				plugins: {
+					tooltip: {
+						enabled: true,
+						intersect: false,
+						mode: "index",
+						titleFont: {
+							family: "Manrope Variable",
+						},
+						bodyFont: {
+							family: "Manrope Variable",
+						},
+					},
+					legend: {
+						position: "bottom",
+						labels: {
+							font: {
+								family: "Manrope Variable",
+							},
+						},
+					},
+					// @ts-ignore
+					crosshair: {
+						line: {
+							width: 0,
+							color: "#000",
+							dashPattern: [6, 4],
+						},
+						zoom: {
+							enabled: false,
+						},
+						sync: {
+							enabled: false,
+						},
+					},
+				},
 				scales: {
+					x: {
+						grid: {
+							display: false,
+						},
+						ticks: {
+							font: {
+								family: "Manrope Variable",
+							},
+							padding: 6,
+						},
+					},
 					y: {
 						beginAtZero: true,
+						grid: {
+							display: true,
+							drawTicks: false,
+							offset: false,
+						},
+						border: {
+							dash: [5, 5],
+							width: 1,
+						},
+						ticks: {
+							font: {
+								family: "Manrope Variable",
+							},
+							padding: 6,
+						},
 					},
 				},
 				maintainAspectRatio: false,
+				responsive: true,
+				normalized: true,
 			},
 		});
 	}
 </script>
 
-<div class="grid gap-4 mt-3 md:grid-cols-2 lg:grid-cols-12">
+<section class="grid gap-4 mt-3 md:grid-cols-2 lg:grid-cols-12">
 	<div class="col-span-8 mt-3">
-		<h2 class="font-bold">Overview</h2>
-		<div class="h-[21rem] 2xl:h-[21rem] border rounded-lg mt-4 p-1">
-			<canvas bind:this={ctx} in:fade />
-		</div>
+		<h2 class="text-lg font-bold mb-6">Overview</h2>
+		<Card.Root>
+			<Card.Header>
+				<Card.Title>Metrics Over Time</Card.Title>
+			</Card.Header>
+			<Card.Content class="h-[400px] md:h-[300px]">
+				<canvas bind:this={ctx} in:fade />
+			</Card.Content>
+		</Card.Root>
 	</div>
-	<div class="pt-5 ms-5 mt-8 col-span-4">
-		<select bind:value={chartType}>
-			<option value="line">Line</option>
-			<option value="bar">Bar</option>
-		</select>
-		<div class="pt-5">
-			<div>
-				<label for="age">
-					<input type="checkbox" bind:checked={showAge} id="age" />
-					<span>Total Manhours</span>
-				</label>
+	<div class="md:pt-5 ms-5 md:mt-11 col-span-4">
+		<Select.Root>
+			<Select.Trigger class="w-[180px]">
+				<Select.Value placeholder="Chart Type" />
+			</Select.Trigger>
+			<Select.Content>
+				<Select.Item
+					value="line"
+					label="Line"
+					on:click={() => (chartType = "line")}>Line</Select.Item
+				>
+				<Select.Item
+					value="bar"
+					label="Bar"
+					on:click={() => (chartType = "bar")}>Bar</Select.Item
+				>
+			</Select.Content>
+		</Select.Root>
+		<div class="pt-5 space-y-3">
+			<div class="flex items-center space-x-2">
+				<Checkbox bind:checked={showAge} id="age" />
+				<Label
+					for="age"
+					id="age-label"
+					class="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+				>
+					Total Manhours
+				</Label>
 			</div>
-			<div>
-				<label for="height">
-					<input type="checkbox" bind:checked={showHeight} id="height" />
-					<span>Total Volunteers</span>
-				</label>
+			<div class="flex items-center space-x-2">
+				<Checkbox bind:checked={showHeight} id="height" />
+				<Label
+					for="height"
+					id="height-label"
+					class="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+				>
+					Total Volunteers
+				</Label>
 			</div>
-			<div>
-				<label for="weight">
-					<input type="checkbox" bind:checked={showWeight} id="weight" />
-					<span>Average Satisfaction</span>
-				</label>
+			<div class="flex items-center space-x-2">
+				<Checkbox bind:checked={showWeight} id="weight" />
+				<Label
+					for="weight"
+					id="weight-label"
+					class="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+				>
+					Average Satisfaction
+				</Label>
 			</div>
 		</div>
 		<div class="pt-5">
 			{#if ctx}
 				<a href={ctx.toDataURL()} target="_blank" download="graph.png">
-					<Button>Download Chart</Button>
+					<Button variant="outline"
+						><Download class="h-4 w-4 mr-3" /> Download</Button
+					>
 				</a>
 			{/if}
 		</div>
 	</div>
-</div>
+</section>
